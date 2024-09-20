d1d4e03fa5869d5cb299b92444ed2a0f1dc21a13
Mason Hunter <masonlizard@gmail.com>
2024-09-20 16:51:52 -0600

    navbar done

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -4,16 +4,16 @@ import Button from '@mui/material/Button';
 const Navbar: React.FC = () => {
     return (
         <nav className="navbar">
-            <Link href="/">LOGO</Link>
+            <Link id="logo" href="/">caster</Link>
             <ul>
                 <li>
-                    <Link href="/about">
-                        <Button className='button-text' variant="text">Log in</Button>
+                    <Link href="/login">
+                        <Button className='button button-text' variant="text">Log in</Button>
                     </Link>
                 </li>
                 <li>
-                    <Link href="/contact">
-                        <Button className='button-contained' variant="contained">Sign up</Button>
+                    <Link href="/signup">
+                        <Button className='button button-contained' variant="contained">Sign up</Button>
                     </Link>
                 </li>
             </ul>
---
+++ src/pages/signup.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function SignUp() {
+    return (
+        <>
+            <Head>
+                <title>Sign up for Caster!</title>
+                <meta name="description" content="Create a free account on Caster to get started with forecasting" />
+            </Head>
+            <div>
+                <h1>Sign Up</h1>
+            </div>
+        </>
+    );
+}
