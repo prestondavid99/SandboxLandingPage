c108192a76a19c3380c9f580219844ca9c36c18d
Mason Hunter <masonlizard@gmail.com>
2024-09-20 12:13:26 -0600

    super basic web app

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer: React.FC = () => {
+    return (
+        <footer>
+            <p>&copy; {new Date().getFullYear()} My Website</p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+// This page is the root of our app.
+// Every other page we build will be a child of this page.
+
+import type { AppProps } from 'next/app';
+import Navbar from '../components/layout/Navbar';
+import Footer from '../components/layout/Footer';
+import '../styles/style.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <div>
+            <Navbar />
+            <main>
+                <Component {...pageProps} />
+            </main>
+            <Footer />
+        </div>
+    );
+}
+
+export default MyApp;
