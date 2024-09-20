830b0304f1dab9cea8c0c40de8f27d68d715fc53
Mason Hunter <masonlizard@gmail.com>
2024-09-20 15:55:24 -0600

    start on navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,17 +1,20 @@
 import Link from 'next/link';
+import Button from '@mui/material/Button';
 
 const Navbar: React.FC = () => {
     return (
-        <nav>
+        <nav className="navbar">
+            <Link href="/">LOGO</Link>
             <ul>
                 <li>
-                    <Link href="/">Home</Link>
+                    <Link href="/about">
+                        <Button className='button-text' variant="text">Log in</Button>
+                    </Link>
                 </li>
                 <li>
-                    <Link href="/about">About</Link>
-                </li>
-                <li>
-                    <Link href="/contact">Contact</Link>
+                    <Link href="/contact">
+                        <Button className='button-contained' variant="contained">Sign up</Button>
+                    </Link>
                 </li>
             </ul>
         </nav>
