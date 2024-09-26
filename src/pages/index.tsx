48e37907191053c29bd449c053399e9f4ace4495
Preston Thomas <prestondthomas72@gmail.com>
2024-09-26 13:27:03 -0600

    added pretty squares (the code isn't pretty though)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,32 +7,38 @@ export default function Home() {
         <>
             <Head>
                 <title>Caster: Create Effortless Forecasts</title>
-                <meta name="description" content="Caster automates the forecasting process for ecommerce stores" />
+                <meta name="description" content="Caster automates the forecasting process for ecommerce stores"/>
             </Head>
-            <div className='home-title-section'>
-                <div className="right-square"></div>
-                <div className='home-title'>
-                    <h1>
-                        AI-Powered <br></br>
-                        <span className='text-accent'>Proforma</span> for <br></br>
-                        eCommerce
-                    </h1>
-                    <p>
-                        Spend less time in Excel and more time <br></br>
-                        growing your business
-                    </p>
+            <div className="home-title-section">
+                <div className="square-container">
+                    <div className="left-square"></div>
                 </div>
-                <div className='home-links'>
-                    <form className="submit-email" action="/submit-email" method="POST">
-                        <input type="email" id="email" name="email" required placeholder="Enter your email"/>
-                    </form>
-                    <Link href="/signup">
-                        <Button className='button button-contained' variant="contained">Join Waitlist</Button>
-                    </Link>
-                    {/*<Link href="/comingsoon">Watch Demo</Link>*/}
+                <div className='home-content-section'>
+                    <div className='home-title'>
+                        <h1>
+                            One-click <br></br>
+                            <span className='text-accent'>forecasts</span> for <br></br>
+                            eCommerce
+                        </h1>
+                        <p>
+                            Spend less time in Excel and more time <br></br>
+                            growing your business
+                        </p>
+                    </div>
+                    <div className='home-links'>
+                        <form className="submit-email" action="/submit-email" method="POST">
+                            <input type="email" id="email" name="email" required placeholder="Enter your email"/>
+                        </form>
+                        <Link href="/signup">
+                            <Button className='button button-contained' variant="contained">Join Waitlist</Button>
+                        </Link>
+                        {/*<Link href="/comingsoon">Watch Demo</Link>*/}
+                    </div>
                 </div>
-                <div className="left-square"></div>
-            </div>
-        </>
-    );
-}
+                <div className="square-container">
+                    <div className="right-square"></div>
+                </div>
+                </div>
+            </>
+            );
+            }
